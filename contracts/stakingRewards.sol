6f1c4625b34735e5e5d01c102346fa6474c8c242
Ibrahim Aziz <ibraziz21@gmail.com>
2023-02-24 09:08:07 +0300

    Update stakingRewards.sol

--- contracts/stakingRewards.sol
+++ contracts/stakingRewards.sol
@@ -1,41 +1,40 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity 0.8.17;
 
 // This contract allows users to stake their ERC20 tokens and receive rewards per second.
 
 contract StakingContract {
-    // Mapping to store user stakes
-    mapping (address => uint256) public userStakes;
+    error Forbidden();
 
-    // Reward pool
+
+    address private owner;
+    uint256 public totalStaked;
     uint256 public rewardPool;
 
-    // Function to allow users to stake their ERC20 tokens
-    function stake(uint256 amount) public {
-        // Update user stake
-        userStakes[msg.sender] += amount;
-        // Update reward pool
-        rewardPool += amount;
+    // Mapping to store user stakes
+    struct StakingInfo{
+        uint amountStaked;
+        uint stepStakedOn;
+    }
+    struct StepInfo {
+        
+    }
+    mapping (address => StakingInfo) public userStakes;
+
+    
+    
+    constructor(){
+        owner = msg.sender;
+    }
+    modifier onlyOwner {
+        if(msg.sender!=owner) revert Forbidden();
+        _;
     }
 
-    // Function to allow users to unstake their ERC20 tokens
-    function unstake(uint256 amount) public {
-        // Check if user has enough tokens staked
-        require(userStakes[msg.sender] >= amount);
-        // Update user stake
-        userStakes[msg.sender] -= amount;
-        // Update reward pool
-        rewardPool -= amount;
+    function fillRewards(uint amount) {
+        
     }
+    
 
-    // Function to calculate rewards per second
-    function calculateRewardsPerSecond() public view returns (uint256) {
-        // Calculate total staked tokens
-        uint256 totalStakedTokens = 0;
-        for (address user in userStakes) {
-            totalStakedTokens += userStakes[user];
-        }
-        // Calculate rewards per second
-        return rewardPool / totalStakedTokens;
-    }
+
 }
