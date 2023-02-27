f8fe41073275ccfeb1f6255a565345e16b97259b
Ibrahim Aziz <ibraziz21@gmail.com>
2023-02-27 10:42:38 +0300

    Update stakingToken.sol

--- contracts/stakingToken.sol
+++ contracts/stakingToken.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
@@ -11,8 +11,11 @@ constructor()ERC20("Stake Token", "STAKE"){
     _mint(msg.sender,1000000*10**18);
 }
 
-function mintReward(uint amount)external {
-    if(amount == 0) revert InvalidInput();
-    _mint(msg.sender,amount);
+function mintReward(address _caller, uint amount)external {
+    if(amount == 0 || _caller == address(0)) revert InvalidInput();
+    _mint(_caller,amount);
 }
+}
+interface IRewards is IERC20 {
+    function mintReward(address _caller, uint amount) external;
 }
